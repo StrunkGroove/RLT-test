2824e68f844dec11d6ba199f95232a0632240102
StrunkGroove <117349896+StrunkGroove@users.noreply.github.com>
2024-04-29 13:44:58 +0300

    Update handler.py

--- app/aggregation_statistic/handler.py
+++ app/aggregation_statistic/handler.py
@@ -6,8 +6,8 @@ from pydantic import ValidationError as PydanticValidationError
 from .services import AggregationOfStatisticalData
 from .schemas import InputData
 
-router = Router()
 
+router = Router()
 
 @router.message()
 async def aggregation_of_statistical_data(message: types.Message) -> None:
@@ -16,19 +16,20 @@ async def aggregation_of_statistical_data(message: types.Message) -> None:
     
     :raises json.JSONDecodeError: if there's an error during JSON decoding.
     """
-
-    agg_data = AggregationOfStatisticalData()
-
     try:
         message_data = json.loads(message.text)
     except json.JSONDecodeError:
         await message.answer("Ошибка при загрузке JSON-данных из сообщения")
-    
+        return
+        
     try:
         input_data = InputData(**message_data)
     except PydanticValidationError:
         await message.answer("Невалидный запос.")
-
+        return
+        
+    agg_data = AggregationOfStatisticalData()
     response = agg_data.main(input_data)
 
     await message.answer(response)
+    
